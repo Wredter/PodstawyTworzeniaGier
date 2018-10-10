b6577660a10907cdeaac05bdd8394c067e8a11df
Wredter <wiktoriusrex@gmail.com>
2018-10-10 20:53:23 +0200

    Prototype
    Scene Wiktor

---
+++ PodstawyTworzeniaGier/Assets/cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+    public GameObject obj;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y,-10);
+
+        offset = transform.position - obj.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = obj.transform.position + offset;
+
+
+    }
+}
