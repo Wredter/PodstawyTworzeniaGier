87e800d8bdc1e21081b4083acccfddecdaf6238b
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-10-10 19:32:30 +0200

    Running and shooting, finally

--- PodstawyTworzeniaGier/Assets/Scripts/minion_temporary_script.cs
+++ PodstawyTworzeniaGier/Assets/Scripts/minion_temporary_script.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 public class minion_temporary_script : MonoBehaviour {
     public float moveSpeed;
     public GameObject projectile;
-    private float maxSpeed = 5f;
     private Vector2 input;
+    private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (GetComponent<Rigidbody2D>().velocity.magnitude < maxSpeed)
-        {
-            GetComponent<Rigidbody2D>().AddForce(input * moveSpeed);
-        }
+        rb2d.velocity = new Vector2(input.x * moveSpeed,input.y * moveSpeed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+            bullet.GetComponent<Rigidbody2D>().AddForce(input * 1000);
         }
-        print(input);
     }
 }
