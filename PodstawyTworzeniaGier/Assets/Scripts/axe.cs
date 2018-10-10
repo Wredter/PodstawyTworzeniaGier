e6869d95319a7d3a504f0001e348815d742cdc8b
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-10-10 22:04:38 +0200

    Bullet mechanichs works, somehow

---
+++ PodstawyTworzeniaGier/Assets/Scripts/axe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class axe : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    private Vector2 initialVelocity;
+    private float step;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        initialVelocity = rb2d.velocity;
+        step = 1.01f;
+	}
+
+    private void FixedUpdate()
+    {
+        if (step == 1)
+        {
+            initialVelocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (step > 0)
+        {
+            rb2d.velocity = initialVelocity * step;
+            step -= 0.01f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
--- PodstawyTworzeniaGier/Assets/Scripts/minion_temporary_script.cs
+++ PodstawyTworzeniaGier/Assets/Scripts/minion_temporary_script.cs
@@ -2,26 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class minion_temporary_script : MonoBehaviour {
+public class minion_temporary_script : MonoBehaviour
+{
     public float moveSpeed;
     public GameObject projectile;
+    public int maxProjectileCount;
     private Vector2 input;
     private Rigidbody2D rb2d;
+    private int maxBulletCount = 3;
+    private int bulletCount;
+    private List<Bullet> bullets;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb2d = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rb2d.velocity = new Vector2(input.x * moveSpeed,input.y * moveSpeed);
+        bullets = new List<Bullet>();
+        bulletCount = 0;
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(input.x * moveSpeed, input.y * moveSpeed);
+        bullets.ForEach(i => i.UpdateCounter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        if (Input.GetKeyDown(KeyCode.Space) && bullets.Count < maxProjectileCount)
         {
-            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody2D>().AddForce(input * 1000);
+            bullets.Add(new Bullet(transform, input, bulletCount, projectile));
+            bulletCount++;
+            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach(Bullet b in bullets)
+        {
+            if(b.GetGameObject().name.Equals(other.gameObject.name) && b.GetCounter() > 40)
+            {
+                Destroy(other.gameObject);
+                bullets.Remove(b);
+                break;
+            }
+        }
+    }
+
+    class Bullet
+    {
+        public GameObject projectile;
+        private GameObject gameObject;
+        private int counter;
+
+        public Bullet(Transform transform, Vector2 input, int bulletCount, GameObject projectile)
+        {
+            gameObject = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(input * 1000);
+            gameObject.name = "bullet" + bulletCount;
+        }
+
+        public GameObject GetGameObject()
+        {
+            return gameObject;
+        }
+
+        public int GetCounter()
+        {
+            return counter;
+        }
+
+        public void UpdateCounter()
+        {
+            counter++;
         }
     }
 }
