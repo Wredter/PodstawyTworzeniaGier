6115960948cf3a0481e8aba452f34b299be7ac4a
Wredter <wiktoriusrex@gmail.com>
2018-10-11 22:22:18 +0200

    NetworkingTest
    Basic Network connections, now we can connect local versions of the game, defoult scene on build set to "Wiktor"

---
+++ PodstawyTworzeniaGier/Assets/Scripts/PlayerLocal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerLocal : NetworkBehaviour {
+    public GameObject PlayerUnitPrefab;
+	// Use this for initialization
+	void Start () {
+        if ( !isLocalPlayer )
+        {
+            return;
+        }
+
+        CmdSpawnMyUnit();
+
+		
+	}
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    /////////////////// COMANDS ///////////////////
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject obj = Instantiate(PlayerUnitPrefab);
+
+        NetworkServer.SpawnWithClientAuthority(obj, connectionToClient);
+    }
+}
