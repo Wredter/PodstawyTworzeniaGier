b6398dd370c5c16ec1515efd6595ae0fdb3fc531
Hyphen133 <hyphen133@gmail.com>
2018-10-19 21:58:02 +0200

    Spartan added

---
+++ PodstawyTworzeniaGier/Assets/ShieldCover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldCover : MonoBehaviour {
+
+    public float maximalShieldTime;
+    public float totalCooldown;
+    private float currentCooldown;
+    private float currentShieldTime;
+    private float currentRotation;
+    private bool doShield;
+
+	// Use this for initialization
+	void Start () {
+        doShield = false;
+        currentCooldown = totalCooldown; //to enable it
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentCooldown >= totalCooldown)
+            {
+                doShield = true;
+                currentShieldTime = 0;
+                currentCooldown = 0;
+                this.transform.Rotate(0, 0, -90);
+            }
+        }
+
+        if(doShield == true)
+        {
+            currentShieldTime += Time.deltaTime;
+            if(currentShieldTime> maximalShieldTime)
+            {
+                doShield = false;
+                this.transform.Rotate(0, 0, 90);
+                currentCooldown = 0;
+            }
+        }
+        else
+        {
+            currentCooldown += Time.deltaTime;
+        }
+        
+        
+    }
+}
