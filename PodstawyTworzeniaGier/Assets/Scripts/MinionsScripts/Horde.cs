63575443976f5791c886ce4ec1fba8f5e16047dc
Wiktor <wiktor.muraszko@wp.pl>
2019-01-23 20:51:05 +0100

    sounds

--- PodstawyTworzeniaGier/Assets/Scripts/MinionsScripts/Horde.cs
+++ PodstawyTworzeniaGier/Assets/Scripts/MinionsScripts/Horde.cs
@@ -248,6 +248,7 @@ public class Horde : MonoBehaviour, IPlayerIntegration
                     {
                         if (divideCooldownTimer <= 0)
                         {
+                            skillSoundSource.PlayOneShot(skillSounds[Random.Range(0, skillSounds.Count)], volume);
                             divide = 12;
                             if (isZombie) divide = 25;
                             center = chief.transform.position;
@@ -258,6 +259,7 @@ public class Horde : MonoBehaviour, IPlayerIntegration
                     {
                         if (divideCooldownTimer <= 0)
                         {
+                            skillSoundSource.PlayOneShot(skillSounds[Random.Range(0, skillSounds.Count)], volume);
                             divide = 12;
                             if (isZombie) divide = 25;
                             center = chief.transform.position;
@@ -269,6 +271,7 @@ public class Horde : MonoBehaviour, IPlayerIntegration
                         Debug.Log("SPARTAN EEEEE");
                         if (spartanTimer <= 0)
                         {
+                            skillSoundSource.PlayOneShot(skillSounds[Random.Range(0, skillSounds.Count)], volume);
                             spartanX = chief.transform.position.x + controller.LookHorizontal() * 15;
                             spartanY = chief.transform.position.y + controller.LookVertical() * 15;
                             isSpartanDash = true;
