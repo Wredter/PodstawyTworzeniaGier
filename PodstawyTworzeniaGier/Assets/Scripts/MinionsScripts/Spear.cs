41aacdbc4208abe048522146ac323817d9c8bed9
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2019-01-26 22:52:27 +0100

    Spartan works

---
+++ PodstawyTworzeniaGier/Assets/Scripts/MinionsScripts/Spear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    public float duration = 0;
+    public float t = 0;
+
+    private Quaternion startRot;
+
+    private void Start()
+    {
+        startRot = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.localRotation = Quaternion.AngleAxis(t / duration * 360f, Vector3.forward); //or transform.right if you want it to be locally based
+            Debug.Log(duration);
+        }
+        else
+        {
+            duration = -1;
+            t = 0.0f;
+            transform.localRotation = transform.parent.rotation;
+        }
+    }
+}
