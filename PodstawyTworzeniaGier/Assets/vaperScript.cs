dd0545226d9877cd0409f283edeaaf3e7c976ad7
AconLight <w.mosieski@interia.pl>
2019-01-27 21:49:08 +0100

    szefy templatka

---
+++ PodstawyTworzeniaGier/Assets/vaperScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vaperScript : MonoBehaviour {
+
+    public GameObject chmurka;
+	// Use this for initialization
+	void Start () {
+        VapTimer = 0;
+    }
+
+    public float VapTime;
+    private float VapTimer;
+    
+	void Update () {
+        VapTimer += Time.deltaTime;
+	}
+
+    public void Vap()
+    {
+        if (VapTimer > VapTime)
+        {
+            VapTime = 0;
+        }
+    }
+}
