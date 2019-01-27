a002fb6f7ee2b2cc98cb6af8a794221f3a4b731c
AconLight <w.mosieski@interia.pl>
2019-01-27 22:28:41 +0100

    vaper

---
+++ PodstawyTworzeniaGier/Assets/ChmurkaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChmurkaScript : MonoBehaviour {
+
+    private float time;
+    private float time2;
+    private float scl;
+    public float doubleT = 3;
+    void Start () {
+        time = 0;
+        time2 = doubleT;
+        scl = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time2 -= Time.deltaTime;
+        time += time2 * Time.deltaTime;
+        scl = time;
+        transform.localScale = new Vector3(scl, scl, 1);
+        if (scl < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- PodstawyTworzeniaGier/Assets/vaperScript.cs
+++ PodstawyTworzeniaGier/Assets/vaperScript.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class vaperScript : MonoBehaviour {
+    IController controller;
+    public void SetController(IController controller)
+    {
+        this.controller = controller;
+    }
 
     public GameObject chmurka;
 	// Use this for initialization
 	void Start () {
         VapTimer = 0;
     }
-
-    public float VapTime;
+    public int vapIStart = 5;
+    public float VapTime = 5;
     private float VapTimer;
+    private float vapDelta;
+    private int vapI;
     
 	void Update () {
         VapTimer += Time.deltaTime;
-	}
+
+        if (vapI > 1)
+        {
+            if (vapI > VapTime - VapTimer*4)
+            {
+                vapI--;
+                Instantiate(chmurka, transform);
+            }
+        }
+
+        if (controller.Special2())
+        {
+            Vap();
+        }
+    }
 
     public void Vap()
     {
         if (VapTimer > VapTime)
         {
-            VapTime = 0;
+            vapI = 5;
+            VapTimer = 0;
         }
+
     }
 }
