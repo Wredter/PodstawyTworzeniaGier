c7efd260139d12c559b2c5f9ef493f0c6f591124
Stanisław Zakrzewski <sabrisv123@gmail.com>
2019-01-28 16:34:11 +0100

    Joystick managing

--- PodstawyTworzeniaGier/Assets/Scripts/ScenesScripts/NextScene.cs
+++ PodstawyTworzeniaGier/Assets/Scripts/ScenesScripts/NextScene.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextScene : MonoBehaviour {
-	void FixedUpdate () {
-		if(Input.GetButton("Joystick1A") || Input.GetButton("Joystick2A") || Input.GetKey(KeyCode.KeypadEnter))
+public class NextScene : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Joystick1A") || Input.GetButton("Joystick2A") || Input.GetKey(KeyCode.KeypadEnter))
         {
-            SceneManager.LoadScene("SelectionScreen");
+            SceneManager.LoadScene("SelectionScreen" + PlayerPrefs.GetInt("PlayersCount") + "Players");
         }
-	}
+    }
 }
